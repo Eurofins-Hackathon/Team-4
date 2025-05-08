917d2bbe49437057cf84f02e60ca351e6def3ee8
Milind <qg2j@eurofins.com>
2025-05-08 09:40:04 +0100

    Add changes to feature branch

--- Battle/Repositories/IGameRepository.cs
+++
@@ -1,9 +0,0 @@
-using BattleshipGame.Models;
-
-namespace BattleshipGame.Repositories;
-
-public interface IGameRepository
-{
-    void SaveGameState(GameState gameState);
-    GameState LoadGameState();
-}
--- Battle/obj/Debug/net9.0/Battle.AssemblyInfo.cs
+++ Battle/obj/Debug/net9.0/Battle.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Battle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bd2ed4077f45fb34acb41746dee96f1f97f43b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Battle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Battle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
